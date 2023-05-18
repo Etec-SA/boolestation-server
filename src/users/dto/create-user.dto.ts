7c5c83f1fe8d6faef9f582676f8b41a0a3b0541f
iamthepoe <luca1901p@gmail.com>
2023-05-18 16:42:50 -0300

    change error message

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,7 +3,9 @@ export class CreateUserDto {
     @IsNotEmpty({ message: 'O nome de usuário não pode estar vazio.' })
     @Length(4, 22, { message: 'O nome de usuário deve ter entre 4 e 20 caracteres.' })
     @Matches(/^[a-zA-Z0-9_-]+$/, {
-        message: 'O username deve conter apenas letras, números, traços e underlines.'
+        message: 'O nome de usuário deve conter apenas letras, números, traços e underlines.'
     })
     username: string;
+    
+
 }
