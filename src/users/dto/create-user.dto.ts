933590c7d19157eac85e434bef0b538ef776658b
iamthepoe <luca1901p@gmail.com>
2023-05-18 17:02:49 -0300

    add birthdate and your validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, Length, Matches } from "class-validator";
+import { IsDate, IsEmail, IsNotEmpty, Length, Matches } from "class-validator";
 export class CreateUserDto {
     @IsNotEmpty({ message: 'O nome de usuário não pode estar vazio.' })
     @Length(4, 22, { message: 'O nome de usuário deve ter entre 4 e 20 caracteres.' })
@@ -13,4 +13,8 @@ export class CreateUserDto {
     @IsNotEmpty({message: 'A senha não pode estar vazia.'})
     @Length(8, 128, {message: 'A senha deve ter entre 8 a 128 caracteres'})
     password: string;
+
+    @IsNotEmpty({message: 'A data de nascimento não pode estar vazia.'})
+    @IsDate({message: 'O valor inserido não é uma data.'})
+    birthdate: Date
 }
