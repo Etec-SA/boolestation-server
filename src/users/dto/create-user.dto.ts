e746de7f61343708d7c3393f369177ffb11b8c1c
iamthepoe <luca1901p@gmail.com>
2023-05-18 16:41:33 -0300

    add username prop and validators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { IsNotEmpty, Length, Matches } from "class-validator";
+export class CreateUserDto {
+    @IsNotEmpty({ message: 'O nome de usuário não pode estar vazio.' })
+    @Length(4, 22, { message: 'O nome de usuário deve ter entre 4 e 20 caracteres.' })
+    @Matches(/^[a-zA-Z0-9_-]+$/, {
+        message: 'O username deve conter apenas letras, números, traços e underlines.'
+    })
+    username: string;
+}
