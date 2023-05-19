57a961e763f51b829a1ac72e98dc2e06ac86e696
iamthepoe <lucagamerbrime@gmail.com>
2023-05-19 10:37:18 -0300

    add requiredXp and your validations

--- src/level-states/dto/create-level-state.dto.ts
+++ src/level-states/dto/create-level-state.dto.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateLevelStateDto {
     @IsNotEmpty({message: 'O título não pode estar vazio.'})
     @IsString({message: 'O título precisa ser uma string.'})
     title: string;
+
+    @IsNotEmpty({message: 'O campo "requiredXp" não pode estar vazio.'})
+    @IsInt({message: 'O campo "requiredXp" precisa ser um número inteiro.'})
+    requiredXp: Number;
 }
