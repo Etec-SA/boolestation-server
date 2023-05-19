1be50cc0e78018fa51c428e34c3f31fc6d134267
iamthepoe <lucagamerbrime@gmail.com>
2023-05-19 10:53:07 -0300

    add levelstate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './database/prisma.service';
+import { LevelStatesModule } from './level-states/level-states.module';
 
 @Module({
-  imports: [],
+  imports: [LevelStatesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
