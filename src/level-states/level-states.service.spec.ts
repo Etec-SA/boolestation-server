1abac1c6244808cdcf63837e15ccf80370f4f23d
iamthepoe <lucagamerbrime@gmail.com>
2023-05-22 14:23:06 -0300

    add prisma service to tests

--- src/level-states/level-states.service.spec.ts
+++ src/level-states/level-states.service.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LevelStatesService } from './level-states.service';
+import { PrismaService } from '../database/prisma.service'
 
 describe('LevelStatesService', () => {
   let service: LevelStatesService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LevelStatesService],
+      providers: [LevelStatesService, PrismaService],
     }).compile();
 
+    prisma = module.get<PrismaService>(PrismaService);
     service = module.get<LevelStatesService>(LevelStatesService);
   });
 
