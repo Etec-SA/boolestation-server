39dfda1b34dbacb7758336346b9be107bee6eed3
iamthepoe <lucagamerbrime@gmail.com>
2023-05-22 14:41:55 -0300

    setting for tests

--- src/level-states/level-states.service.ts
+++ src/level-states/level-states.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/database/prisma.service';
+import { PrismaService } from '../database/prisma.service';
 import { CreateLevelStateDto } from './dto/create-level-state.dto';
 import { UpdateLevelStateDto } from './dto/update-level-state.dto';
 
