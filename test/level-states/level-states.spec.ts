f1682fd2f3e0b739a626de497181585020c7e015
iamthepoe <luca1901p@gmail.com>
2023-07-02 20:01:21 -0300

    e2e

---
+++ test/level-states/level-states.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { LevelStatesModule } from 'src/level-states/level-states.module';
+
+describe('LevelStatesController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [LevelStatesModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/level-states (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/level-states')
+      .expect(200);
+  });
+
+});
+
