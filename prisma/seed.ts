63e80243521117daaa8fe4d96e998e88cbeed550
iamthepoe <luca1901p@gmail.com>
2023-07-02 19:46:19 -0300

    add items to seed

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+
+  const levelState01 = await prisma.levelState.upsert({
+    where: { id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a' },
+    update: {},
+    create: {
+      id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a',
+      title: 'O Aprendiz',
+      requiredXp: 100
+    }
+  });
+
+  const levelState02 = await prisma.levelState.upsert({
+    where: { id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511' },
+    update: {},
+    create: {
+      id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511',
+      title: 'Estudante Proposicional',
+      requiredXp: 200
+    }
+  });
+
+  console.log(levelState01, levelState02);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
