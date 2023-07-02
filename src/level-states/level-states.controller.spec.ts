290fb5565cd1588782c351624caffb3630d7ade5
iamthepoe <luca1901p@gmail.com>
2023-07-02 13:53:09 -0300

    find all test

--- src/level-states/level-states.controller.spec.ts
+++ src/level-states/level-states.controller.spec.ts
@@ -1,6 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LevelStatesController } from './level-states.controller';
 import { LevelStatesService } from './level-states.service';
+import { LevelState } from '@prisma/client';
+
+const InMemoryLevelStates: Array<LevelState> = [
+  {
+    id: '1c937b93-b38e-47dd-9fe8-a99b9802ed9e',
+    title: 'Aprendiz',
+    requiredXp: 100
+  },
+  {
+    id: '1c937b93-b38e-47dd-9fe8-a99b9802ed9e',
+    title: 'Curioso',
+    requiredXp: 200
+  },
+  {
+    id: '45a215cd-0ad1-48ae-89b3-cb429bf86512',
+    title: 'Mago da Lógica',
+    requiredXp: 400
+  },
+  {
+    id: '246bbc9a-67fa-4547-89a2-00f12ea6c5b7',
+    title: 'O Logicista',
+    requiredXp: 800
+  },
+]
 
 describe('LevelStatesController', () => {
   let controller: LevelStatesController;
@@ -12,7 +36,7 @@ describe('LevelStatesController', () => {
       providers: [{
         provide: LevelStatesService,
         useValue: {
-          findAll: jest.fn(),
+          findAll: jest.fn().mockResolvedValue(InMemoryLevelStates),
           findOne: jest.fn(),
           update: jest.fn(),
           create: jest.fn(),
@@ -28,5 +52,15 @@ describe('LevelStatesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a level state entity successfully', async () => {
+      const result = await controller.findAll();
+
+      expect(result).toEqual(InMemoryLevelStates);
+      expect(service.findAll).toHaveBeenCalledTimes(1);
+      expect(service.findAll).toHaveBeenCalledWith(/* nothing */);
+    });
   });
 });
