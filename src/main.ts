cb87c2f7882c2202df0ede427687e497dd683918
iamthepoe <luca1901p@gmail.com>
2023-07-03 20:31:33 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Boolestation')
+    .setDescription('Server application of Boolestation')
+    .setVersion('1.0')
+    .addTag('boolestation')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3000);
