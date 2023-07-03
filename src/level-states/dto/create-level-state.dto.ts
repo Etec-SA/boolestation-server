762073539c922281752ae043bc286e2097ecb3f9
iamthepoe <luca1901p@gmail.com>
2023-07-03 20:36:15 -0300

    add api props

--- src/level-states/dto/create-level-state.dto.ts
+++ src/level-states/dto/create-level-state.dto.ts
@@ -1,11 +1,15 @@
 import { IsInt, IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLevelStateDto {
+
   @IsNotEmpty({ message: 'O título não pode estar vazio.' })
   @IsString({ message: 'O título precisa ser uma string.' })
+  @ApiProperty()
   title: string;
 
   @IsNotEmpty({ message: 'O campo "requiredXp" não pode estar vazio.' })
   @IsInt({ message: 'O campo "requiredXp" precisa ser um número inteiro.' })
+  @ApiProperty()
   requiredXp: number;
 }
