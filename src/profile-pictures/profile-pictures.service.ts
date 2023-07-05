5380d6e78c5684006e504e8c1e301ea427a32f90
iamthepoe <luca1901p@gmail.com>
2023-07-05 14:43:51 -0300

    create method

--- src/profile-pictures/profile-pictures.service.ts
+++ src/profile-pictures/profile-pictures.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProfilePictureDto } from './dto/create-profile-picture.dto';
 import { UpdateProfilePictureDto } from './dto/update-profile-picture.dto';
-
+import { PrismaService } from '../database/prisma.service';
 @Injectable()
 export class ProfilePicturesService {
-  create(createProfilePictureDto: CreateProfilePictureDto) {
-    return 'This action adds a new profilePicture';
+  constructor(private prisma: PrismaService) { }
+
+  async create(data: CreateProfilePictureDto) {
+    const profilePicture = await this.prisma.profilePicture.create({ data });
+
+    return profilePicture;
   }
 
   findAll() {
