618ba4e2b97ce33adb5d8d8f44e21d03ecadb849
iamthepoe <luca1901p@gmail.com>
2023-07-05 14:20:38 -0300

    inmemory profile pictures

--- src/profile-pictures/profile-pictures.service.spec.ts
+++ src/profile-pictures/profile-pictures.service.spec.ts
@@ -1,5 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ProfilePicture } from '@prisma/client';
 import { ProfilePicturesService } from './profile-pictures.service';
+
+const InMemoryProfilePictures: Array<ProfilePicture> = [
+  {
+    id: '1c937b93-b38e-47dd-9fe8-a99b9802ed9e',
+    title: 'Aristotle',
+    url: 'http://boolestation.com/public/aristotle.png'
+  },
+  {
+    id: '45a215cd-0ad1-48ae-89b3-cb429bf86512',
+    title: 'Immanuel Kant',
+    url: 'http://boolestation.com/public/kant.png'
+  },
+  {
+    id: '2c937b93-b38e-47dd-9fe8-a99b9802ed9e',
+    title: 'Russel',
+    url: 'http://boolestation.com/public/russel.png'
+  },
+  {
+    id: '3c937b93-b38e-47dd-9fe8-a99b9802ed9e',
+    title: 'Frege',
+    url: 'http://boolestation.com/public/frege.png'
+  },
+]
+
+
 
 describe('ProfilePicturesService', () => {
   let service: ProfilePicturesService;
