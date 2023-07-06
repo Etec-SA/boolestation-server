907ba1ecb1f548bdeca46423d457e3601bbe1080
iamthepoe <luca1901p@gmail.com>
2023-07-06 17:26:14 -0300

    add props to dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,27 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, IsNotEmpty, IsString } from "class-validator";
+import { IsDate, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({ example: '@devgamon', description: 'User identification name' })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'Gabriel Gamon', description: 'User geral name' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'devgamon@bool.com ', description: 'User email' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'ḱssw1Z2@oa.ms', description: 'User password' })
   @IsNotEmpty()
   @IsString()
   password: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: '1985-07-15', description: 'User birthdate' })
   @IsNotEmpty()
   @IsDate()
   birthdate: Date;
