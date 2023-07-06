30fb5339e9b28dde82e2b2a23ff1fc7dbc9b016f
iamthepoe <luca1901p@gmail.com>
2023-07-06 00:11:35 -0300

    GET :id

--- test/profile-pictures.e2e-spec.ts
+++ test/profile-pictures.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { ProfilePicturesModule } from 'src/profile-pictures/profile-pictures.module';
+import { ProfilePicturesModule } from '../src/profile-pictures/profile-pictures.module';
 import { ProfilePicture } from '@prisma/client';
 import * as profilePicturesJson from './fixtures/profile-pictures';
 
@@ -28,6 +28,28 @@ describe('AppController (e2e)', () => {
       expect(response.status).toEqual(200);
       expect(typeof response.body).toEqual(typeof profilePicturesJson.data);
       findedProfilePicture = response.body[0];
+    });
+  });
+
+  describe('/profile-pictures/:id (GET)', () => {
+    it('should get a single profile picture', async () => {
+      const id = findedProfilePicture.id;
+
+      let response = await request(app.getHttpServer())
+        .get(`/profile-pictures/${id}`);
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(findedProfilePicture);
+    });
+
+    it('should return a 404', async () => {
+
+      let response = await request(app.getHttpServer())
+        .get(`/profile-pictures/eusimplesmentenaoexisto`);
+
+      expect(response).toBeDefined();
+      expect(response.status).toEqual(404);
     });
   });
 });
