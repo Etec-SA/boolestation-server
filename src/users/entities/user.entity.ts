1882e9562b8ed0d7d4a2a16aecf62fd39f0d7497
iamthepoe <luca1901p@gmail.com>
2023-07-06 17:31:07 -0300

    implement entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  birthdate: Date;
+
+  @ApiProperty()
+  xp: number;
+
+  @ApiProperty()
+  isPremium: boolean;
+
+  @ApiProperty()
+  levelStateId: string;
+
+  @ApiProperty()
+  profilePictureId: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
