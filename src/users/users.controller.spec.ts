8d298919cca91ad6d76a8b809e48ab9246ffafab
iamthepoe <luca1901p@gmail.com>
2023-07-08 23:53:25 -0300

    mock prisma

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+
+const prismaMock  = {
+  create: jest.fn((data: CreateUserDto)=>{
+    return {
+      ...data,
+      password: undefined
+    }
+  })
+}
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +19,9 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService,
+        {provide: PrismaService, useValue: prismaMock}
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
