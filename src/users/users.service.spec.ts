6d9fb8a238c164e032ecad28d172599262a95713
iamthepoe <luca1901p@gmail.com>
2023-07-09 19:12:52 -0300

    create test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,11 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from '../database/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 const prismaMock = {
-  create: jest.fn((data: CreateUserDto) => {
-    return { data };
-  })
+  user: {
+    create: jest.fn((data: CreateUserDto) => {
+      return { ...data, id: crypto.randomUUID() };
+    })
+  },
+  profilePicture: {
+    findFirst: jest.fn().mockResolvedValue(crypto.randomUUID())
+  },
+  levelState: {
+    findFirst: jest.fn().mockResolvedValue(crypto.randomUUID())
+  }
 }
 
 describe('UsersService', () => {
@@ -29,4 +38,25 @@ describe('UsersService', () => {
     expect(prisma).toBeDefined();
   });
 
+  describe('create', () => {
+    it('should create an user with a hashed password', async () => {
+      const user: CreateUserDto = {
+        birthdate: new Date('1985-10-10'),
+        name: 'George Bool',
+        email: 'george@bool.com',
+        password: '123321',
+        username: 'georgebool'
+      };
+
+      jest.spyOn(bcrypt, 'hash').mockImplementation((pass, salt) => {
+        Promise.resolve('');
+      });
+
+      const response = await service.create(user);
+
+      expect(response).toBeDefined();
+      expect(response.id).toBeDefined();
+      expect(response?.password).toBeUndefined();
+    })
+  })
 });
