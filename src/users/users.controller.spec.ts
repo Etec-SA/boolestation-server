d0770d61dae1fcb8e77c06a3e6032f5327ec06ef
iamthepoe <luca1901p@gmail.com>
2023-07-12 19:05:06 -0300

    email test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,6 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
+import { BadRequestException } from '@nestjs/common';
 
 const serviceMock = {
   create: jest.fn((data: CreateUserDto) => {
@@ -59,7 +60,20 @@ describe('UsersController', () => {
     });
 
     it('should return an error if email is already in use', async () => {
-      jest.spyOn()
+      jest.spyOn(service, 'create').mockRejectedValue(new BadRequestException('Email is already in use.'));
+      try {
+        await controller.create({
+          name: 'Luca Poe',
+          birthdate: new Date('2006-01-19'),
+          email: 'lucapoe@bool.com',
+          password: 'iamthepoe',
+          username: 'lucapoe'
+        });
+      } catch (e) {
+        expect(e).toBeInstanceOf(BadRequestException);
+        expect(e.message).toEqual('Email is already in use.');
+      }
+
     });
   });
 });
