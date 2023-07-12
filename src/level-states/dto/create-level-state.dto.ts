4c3fbad5cd71c9bdb762e4739fae8d2128bcc601
iamthepoe <luca1901p@gmail.com>
2023-07-12 19:41:26 -0300

    remove messages

--- src/level-states/dto/create-level-state.dto.ts
+++ src/level-states/dto/create-level-state.dto.ts
@@ -4,12 +4,12 @@ import { IsInt, IsNotEmpty, IsString } from "class-validator";
 export class CreateLevelStateDto {
 
   @ApiProperty({ description: 'The title serves to name the level in which the user is.', example: 'O Logicista' })
-  @IsNotEmpty({ message: 'O título não pode estar vazio.' })
-  @IsString({ message: 'O título precisa ser uma string.' })
+  @IsNotEmpty()
+  @IsString()
   title: string;
 
   @ApiProperty({ description: 'is the amount of experience points the user needs to reach the level', example: 151 })
-  @IsNotEmpty({ message: 'O campo "requiredXp" não pode estar vazio.' })
-  @IsInt({ message: 'O campo "requiredXp" precisa ser um número inteiro.' })
+  @IsNotEmpty()
+  @IsInt()
   requiredXp: number;
 }
