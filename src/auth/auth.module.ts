e0c4d76c6c2dd62a88c33ac59fa8ee66d39a91dc
iamthepoe <luca1901p@gmail.com>
2023-07-15 14:52:08 -0300

    add jwt config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '30d' }
+  })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
