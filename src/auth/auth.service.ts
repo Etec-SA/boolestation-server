ac1af47e08736589c6b92d3f21c0754e3d2e8bcb
iamthepoe <luca1901p@gmail.com>
2023-07-15 13:04:17 -0300

    add user service to constructor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService){}
+    validateUser(email: string, password: string) {
+        throw new Error('Method not implemented.');
+    }
+}
