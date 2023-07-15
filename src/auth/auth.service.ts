f5b205769f95a628d59235a290f9669bc961e65c
iamthepoe <luca1901p@gmail.com>
2023-07-15 14:52:32 -0300

    login method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { UserPayload } from './entities/user-payload.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UsersService){}
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
+    ){}
+
+    async login(user: UserEntity) {
+        const {username, email, name, id} = user;
+
+        const payload: UserPayload = {
+            username,
+            email,
+            name,
+            sub: id
+        }
+
+        const token = this.jwtService.sign(payload);
+
+        return{
+            access_token: token
+        }
+    }
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email);
