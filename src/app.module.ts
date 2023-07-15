dfd9779c3b2bf74994952e2fc4a2211b1c3ad84f
iamthepoe <luca1901p@gmail.com>
2023-07-15 12:40:37 -0300

    reate auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './database/prisma.service';
 import { LevelStatesModule } from './level-states/level-states.module';
 import { ProfilePicturesModule } from './profile-pictures/profile-pictures.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [LevelStatesModule, ProfilePicturesModule, UsersModule],
+  imports: [LevelStatesModule, ProfilePicturesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
