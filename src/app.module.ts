0762f8a6fb2da66166a8734838cd07fa8e880f17
iamthepoe <luca1901p@gmail.com>
2023-07-15 16:26:56 -0300

    add guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,16 @@ import { LevelStatesModule } from './level-states/level-states.module';
 import { ProfilePicturesModule } from './profile-pictures/profile-pictures.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [LevelStatesModule, ProfilePicturesModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService,
+  {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard
+  }],
 })
 export class AppModule {}
