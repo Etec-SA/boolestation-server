26234bfe0d10b78c2f635a96835691d7b9cf8fd7
iamthepoe <luca1901p@gmail.com>
2023-07-15 14:22:21 -0300

    validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UsersService){}
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+        
+        if(!user) throw new Error('Email or password is incorrect.');
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect) throw new Error('Email or password is incorrect.');
+
+        return{
+            ...user,
+            password: undefined
+        }
     }
 }
