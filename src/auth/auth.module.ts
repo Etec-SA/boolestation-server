ce3ce0410ea05d44323787bfb2b80a2f4eb10240
iamthepoe <luca1901p@gmail.com>
2023-07-15 12:58:14 -0300

    Import user module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
