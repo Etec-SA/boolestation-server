b885e3ba4d926d941a69c6c06266f88b3ff2f473
iamthepoe <luca1901p@gmail.com>
2023-07-15 16:09:56 -0300

    strategy for jwt

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserFromJwt } from '../entities/user-jwt.entity';
+import { UserPayload } from '../entities/user-payload.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: UserPayload): Promise<UserFromJwt> {
+    return {
+      id: payload.sub,
+      email: payload.email,
+      username: payload.username,
+      name: payload.name,
+    };
+  }
+}
