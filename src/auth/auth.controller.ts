bd16bb37cc436e6ba40a3549cd256e747615b486
iamthepoe <luca1901p@gmail.com>
2023-07-15 12:43:07 -0300

    endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+    
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    login(){
+      return 'login';
+    }
 }
