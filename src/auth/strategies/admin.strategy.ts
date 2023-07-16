dbf850310d30cf7bb4fb541ea5f7a932fca6e6f5
iamthepoe <luca1901p@gmail.com>
2023-07-16 19:40:00 -0300

    validate

--- src/auth/strategies/admin.strategy.ts
+++ src/auth/strategies/admin.strategy.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UserFromJwt } from '../entities/user-jwt.entity';
 import { UserPayload } from '../entities/user-payload.entity';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AdminStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,12 +14,8 @@ export class AdminStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: UserPayload): Promise<UserFromJwt> {
-    return {
-      id: payload.sub,
-      email: payload.email,
-      username: payload.username,
-      name: payload.name,
-    };
+  async validate(payload: UserPayload): Promise<Boolean> {
+    const id = payload.sub;
+    return await this.authService.validateAdmin(id);
   }
 }
