e54cb6d589322486b56577a30812038751765ba1
iamthepoe <luca1901p@gmail.com>
2023-07-17 20:09:45 -0300

    remove console.log

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,55 +4,54 @@ import * as bcrypt from 'bcrypt';
 import { UserEntity } from 'src/users/entities/user.entity';
 import { UserPayload } from './entities/user-payload.entity';
 import { JwtService } from '@nestjs/jwt';
-import { Role } from './enums/roles.enum';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userService: UsersService,
-        private readonly jwtService: JwtService
-    ){}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService
+  ) { }
 
-    async login(user: UserEntity) {
-        const {username, email, name, id, isAdmin, isPremium} = user;
+  async login(user: UserEntity) {
+    const { username, email, name, id, isAdmin, isPremium } = user;
 
-        const payload: UserPayload = {
-            username,
-            email,
-            name,
-            roles: {
-                isAdmin,
-                isPremium
-            },
-            sub: id
-        }
-
-        const token = this.jwtService.sign(payload);
-
-        return{
-            access_token: token
-        }
+    const payload: UserPayload = {
+      username,
+      email,
+      name,
+      roles: {
+        isAdmin,
+        isPremium
+      },
+      sub: id
     }
 
-    async validateUser(email: string, password: string) {
-        const user = await this.userService.findByEmail(email);
-        
-        if(!user) throw new Error('Email or password is incorrect.');
+    const token = this.jwtService.sign(payload);
 
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-        if(!isPasswordCorrect) throw new Error('Email or password is incorrect.');
-
-        return{
-            ...user,
-            password: undefined
-        }
+    return {
+      access_token: token
     }
+  }
 
-    async validateAdmin(id: string){
-        const user = await this.userService.findOne(id);
-        console.log(user.isAdmin);
-        if(!user.isAdmin) throw new UnauthorizedException('You dont have permission.');
-        return true;
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) throw new Error('Email or password is incorrect.');
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) throw new Error('Email or password is incorrect.');
+
+    return {
+      ...user,
+      password: undefined
     }
+  }
+
+  async validateAdmin(id: string) {
+    const user = await this.userService.findOne(id);
+    console.log(user.isAdmin);
+    if (!user.isAdmin) throw new UnauthorizedException('You dont have permission.');
+    return true;
+  }
 }
