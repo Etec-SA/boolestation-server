171f02a7536c6478e0b85f9d8178f0ed49ad86b1
iamthepoe <luca1901p@gmail.com>
2023-07-18 20:38:25 -0300

    api response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,9 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { Role } from '../auth/enums/roles.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
+import { UserEntity } from './entities/user.entity';
 
 @ApiBearerAuth()
 @ApiTags('users')
@@ -13,30 +14,35 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Roles(Role.Admin)
+  @ApiCreatedResponse({ type: UserEntity })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Roles(Role.Admin)
+  @ApiOkResponse({ type: UserEntity, isArray: true })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
   @Roles(Role.Admin)
+  @ApiOkResponse({ type: UserEntity })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Roles(Role.Admin)
+  @ApiOkResponse({ type: UserEntity })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Roles(Role.Admin)
+  @ApiOkResponse({ type: UserEntity })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
