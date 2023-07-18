53011a9daaf797c3859223c3e0c176a17d6ee0f0
iamthepoe <luca1901p@gmail.com>
2023-07-18 13:59:27 -0300

    implement decorator

---
+++ src/auth/dto/LoginDTO.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
+export class LoginDTO {
+  @ApiProperty({ example: 'luca@poe.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: '¢³12910zna' })
+  @IsString()
+  password: string;
+}
