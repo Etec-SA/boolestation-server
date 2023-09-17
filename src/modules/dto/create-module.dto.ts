82ee4a348be023dbf869368f3e5f345d8407bc10
iamthepoe <luca1901p@gmail.com>
2023-09-16 21:46:19 -0300

    feat: implement DTOs and Entity

--- src/modules/dto/create-module.dto.ts
+++ src/modules/dto/create-module.dto.ts
@@ -1,1 +1,14 @@
-export class CreateModuleDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateModuleDto {
+  @ApiProperty({ example: 'Lógica Proposicional' })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({ example: 'Disciplina introdutória da Lógica Clássica, onde aprenderemos sobre proposições, tabelas-verdade, etc.' })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
--- src/modules/entities/module.entity.ts
+++ src/modules/entities/module.entity.ts
@@ -1,1 +1,10 @@
-export class Module {}
+import { Module } from '@prisma/client';
+
+export class ModuleEntity implements Module {
+  id: string;
+  slug: string;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+  description: string;
+}
