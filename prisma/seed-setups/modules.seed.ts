008c938920893254480383d0a586f55a6f629701
iamthepoe <luca1901p@gmail.com>
2023-09-17 23:42:03 -0300

    add seed and documentation for modules

---
+++ prisma/seed-setups/modules.seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const setupModules = async () => {
+  await prisma.module.upsert({
+    where: { id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a' },
+    update: {},
+    create: {
+      id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a',
+      title: 'Lógica Aristotélica',
+      slug: 'logica-aristotelica',
+      description: 'Módulo onde aprenderemos sobre teoria dos silogismos',
+      createdAt: new Date(Date.UTC(2023, 1, 2)),
+      updatedAt: new Date(Date.UTC(2023, 1, 2)),
+    }
+  });
+
+  await prisma.module.upsert({
+    where: { id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511' },
+    update: {},
+    create: {
+      id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511',
+      title: 'Lógica Proposicional',
+      slug: 'logica-proposicional',
+      description: 'Módulo onde aprenderemos sobre a lógica clássica criada por Frege.',
+      createdAt: new Date(Date.UTC(2023, 1, 2)),
+      updatedAt: new Date(Date.UTC(2023, 1, 2)),
+    }
+  });
+}
+
+export { setupModules };
+
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,12 +2,14 @@ import { setupLevelStates } from './seed-setups/level-state.seed';
 import { setupProfilePictures } from './seed-setups/profile-picture.seed';
 import { PrismaClient } from '@prisma/client';
 import { setupUsers } from './seed-setups/user.seed';
+import { setupModules } from './seed-setups/modules.seed';
 const prisma = new PrismaClient();
 
 async function main() {
   await setupLevelStates();
   await setupProfilePictures();
   await setupUsers();
+  await setupModules();
 }
 
 main()
