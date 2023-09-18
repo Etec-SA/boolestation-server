063bdb5ade099eecb77eb36e88b26b63c1ba9fb8
iamthepoe <luca1901p@gmail.com>
2023-09-17 23:54:56 -0300

    create lesson dto

--- src/lessons/dto/create-lesson.dto.ts
+++ src/lessons/dto/create-lesson.dto.ts
@@ -1,1 +1,20 @@
-export class CreateLessonDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+export class CreateLessonDto {
+  @ApiProperty({ example: 'Operadores - Negação' })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({ example: 'Aprenderemos sobre o operador unário de negação, seu valor semântico e como ele se aplica na linguagem natural' })
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUUID()
+  moduleId: string;
+}
--- src/lessons/entities/lesson.entity.ts
+++ src/lessons/entities/lesson.entity.ts
@@ -1,1 +1,1 @@
-export class Lesson {}
+export class LessonEntity { }
