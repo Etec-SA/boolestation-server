d5ad0f870afe6333d73cd77f8428c8aa21cf9494
iamthepoe <luca1901p@gmail.com>
2023-09-17 23:47:27 -0300

    add prisma service to lessons module

--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
+import { PrismaService } from '../database/prisma.service';
 
 @Module({
   controllers: [LessonsController],
-  providers: [LessonsService]
+  providers: [LessonsService, PrismaService]
 })
-export class LessonsModule {}
+export class LessonsModule { }
