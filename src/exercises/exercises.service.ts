5eb6626dec4e46980384dd02015a7451a7f507d3
iamthepoe <luca1901p@gmail.com>
2023-09-20 18:44:34 -0300

    add prisma to constructor

--- src/exercises/exercises.service.ts
+++ src/exercises/exercises.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
 
 @Injectable()
 export class ExercisesService {
+  constructor(private readonly prisma: PrismaService) { }
+
   create(createExerciseDto: CreateExerciseDto) {
     return 'This action adds a new exercise';
   }
