23c28563dcdf1fc34dbf36426e148e60c6b12d34
iamthepoe <luca1901p@gmail.com>
2023-09-20 18:43:47 -0300

    add prisma service to providers

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { ExercisesController } from './exercises.controller';
+import { PrismaService } from '../database/prisma.service';
 
 @Module({
   controllers: [ExercisesController],
-  providers: [ExercisesService]
+  providers: [ExercisesService, PrismaService]
 })
-export class ExercisesModule {}
+export class ExercisesModule { }
