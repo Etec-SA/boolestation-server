a4c48056e744d137d1b170945ef1e840cd511085
iamthepoe <luca1901p@gmail.com>
2023-09-30 18:08:12 -0300

    create entity

--- src/exercises/entities/exercise.entity.ts
+++ src/exercises/entities/exercise.entity.ts
@@ -1,1 +1,10 @@
-export class Exercise {}
+import { Exercise } from "@prisma/client";
+
+export class ExerciseEntity implements Exercise {
+  id: string;
+  title: string;
+  description: string;
+  lessonId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
