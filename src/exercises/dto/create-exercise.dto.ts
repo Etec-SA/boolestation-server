37412bb73eff752790b2fb93d97d454d3fd05a68
iamthepoe <luca1901p@gmail.com>
2023-09-30 18:07:34 -0300

    exercise DTO

--- src/exercises/dto/create-exercise.dto.ts
+++ src/exercises/dto/create-exercise.dto.ts
@@ -1,1 +1,18 @@
-export class CreateExerciseDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+export class CreateExerciseDto {
+
+  @ApiProperty({ example: 'Eliminando duplas negações' })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({ example: 'Dada a formula (¬¬¬P ^ ¬¬Q), qual seria sua simplificação sem duplas negações?' })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsUUID()
+  lessonId: string;
+}
