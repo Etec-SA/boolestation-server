db999203820217704bc9bd0b8f8443de57bcd063
iamthepoe <luca1901p@gmail.com>
2023-09-30 18:22:14 -0300

    fix exported service

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { ExercisesController } from './exercises.controller';
 import { PrismaService } from '../database/prisma.service';
+import { ModulesModule } from '../modules/modules.module';
 
 @Module({
   controllers: [ExercisesController],
-  providers: [ExercisesService, PrismaService]
+  providers: [ExercisesService, PrismaService],
+  imports: [ModulesModule]
 })
 export class ExercisesModule { }
--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -3,12 +3,11 @@ import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
 import { PrismaService } from '../database/prisma.service';
 import { ModulesModule } from '../modules/modules.module';
-import { ModulesService } from 'src/modules/modules.service';
 
 @Module({
   controllers: [LessonsController],
   providers: [LessonsService, PrismaService],
   imports: [ModulesModule],
-  exports: [ModulesService]
+  exports: [LessonsService]
 })
 export class LessonsModule { }
