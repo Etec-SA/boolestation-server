938d8767698f41f761b43768989b8d86b8d99dd5
iamthepoe <luca1901p@gmail.com>
2023-09-30 18:21:18 -0300

    export lesson service

--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -3,10 +3,12 @@ import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
 import { PrismaService } from '../database/prisma.service';
 import { ModulesModule } from '../modules/modules.module';
+import { ModulesService } from 'src/modules/modules.service';
 
 @Module({
   controllers: [LessonsController],
   providers: [LessonsService, PrismaService],
-  imports: [ModulesModule]
+  imports: [ModulesModule],
+  exports: [ModulesService]
 })
 export class LessonsModule { }
