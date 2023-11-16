78ac012b6ea651ef55e6e1c336611de91bae0864
iamthepoe <luca1901p@gmail.com>
2023-11-16 18:46:11 -0300

    refact: change to Promise.all

--- prisma/seed-setups/profile-picture.seed.ts
+++ prisma/seed-setups/profile-picture.seed.ts
@@ -1,26 +1,29 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const setupProfilePictures = async ()=>{
-    await prisma.profilePicture.upsert({
-        where: { id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a' },
-        update: {},
-        create: {
-          id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a',
-          title: 'Aristotle',
-          url: 'https://cdn.thecollector.com/wp-content/uploads/2023/05/francesco-hayez-aristotle-painting.jpg'
-        }
-      });
-    
-      await prisma.profilePicture.upsert({
-        where: { id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511' },
-        update: {},
-        create: {
-            id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511',
-            title: 'Plato',
-            url: 'https://images.saymedia-content.com/.image/t_share/MTgwMDE1OTM1MjA4NjI5Mzcw/the-ancient-greek-philosopher-plato-his-life-and-works.jpg'
-          }
-      });
-}
+const setupProfilePictures = async () => {
+  const promises = [
+    prisma.profilePicture.upsert({
+      where: { id: "4151e54b-8517-4871-a1d2-acfb67a8bf1a" },
+      update: {},
+      create: {
+        id: "4151e54b-8517-4871-a1d2-acfb67a8bf1a",
+        title: "Aristotle",
+        url: "https://cdn.thecollector.com/wp-content/uploads/2023/05/francesco-hayez-aristotle-painting.jpg",
+      },
+    }),
+    prisma.profilePicture.upsert({
+      where: { id: "af0f6b40-d98d-4a77-a1a1-3fdfff114511" },
+      update: {},
+      create: {
+        id: "af0f6b40-d98d-4a77-a1a1-3fdfff114511",
+        title: "Plato",
+        url: "https://images.saymedia-content.com/.image/t_share/MTgwMDE1OTM1MjA4NjI5Mzcw/the-ancient-greek-philosopher-plato-his-life-and-works.jpg",
+      },
+    }),
+  ];
 
-export {setupProfilePictures};
+  await Promise.all(promises);
+};
+
+export { setupProfilePictures };
--- prisma/seed-setups/user.seed.ts
+++ prisma/seed-setups/user.seed.ts
@@ -1,41 +1,47 @@
-import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
-import * as dayjs from 'dayjs';
-import * as utc from 'dayjs/plugin/utc';
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
+import * as dayjs from "dayjs";
+import * as utc from "dayjs/plugin/utc";
 
 const prisma = new PrismaClient();
 dayjs.extend(utc);
 
-const setupUsers = async ()=>{
-    await prisma.user.upsert({
-        where: { id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a' },
-        update: {},
-        create: {
-          id: '4151e54b-8517-4871-a1d2-acfb67a8bf1a',
-          name: 'Aristotle',
-          username: 'aristotle',
-          email: 'aristotle@athenas.com',
-          birthdate: dayjs('1999-11-11').utc().format(),
-          levelStateId: '4151e54b-8517-4871-a1d2-acfb67a8bf1a',
-          profilePictureId: '4151e54b-8517-4871-a1d2-acfb67a8bf1a',
-          password: await bcrypt.hash('zonpoNWDNIEONAwqplaźwdqwd-20@(13142', 10),
-        }
-      });
-    
-    await prisma.user.upsert({
-        where: { id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511' },
-        update: {},
-        create: {
-            id: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511',
-            name: 'Plato',
-            username: 'plato',
-            email: 'plato@athenas.com',
-            birthdate: dayjs('1999-11-11').utc().format(),
-            levelStateId: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511',
-            profilePictureId: 'af0f6b40-d98d-4a77-a1a1-3fdfff114511',
-            password: await bcrypt.hash('zonpoNWDNIEONAwqplaźwdqwd-20@(1314sda21342', 10),
-          }
-      });
-}
+const setupUsers = async () => {
+  const promises = [
+    prisma.user.upsert({
+      where: { id: "4151e54b-8517-4871-a1d2-acfb67a8bf1a" },
+      update: {},
+      create: {
+        id: "4151e54b-8517-4871-a1d2-acfb67a8bf1a",
+        name: "Aristotle",
+        username: "aristotle",
+        email: "aristotle@athenas.com",
+        birthdate: dayjs("1999-11-11").utc().format(),
+        levelStateId: "4151e54b-8517-4871-a1d2-acfb67a8bf1a",
+        profilePictureId: "4151e54b-8517-4871-a1d2-acfb67a8bf1a",
+        password: await bcrypt.hash("zonpoNWDNIEONAwqplaźwdqwd-20@(13142", 10),
+      },
+    }),
+    prisma.user.upsert({
+      where: { id: "af0f6b40-d98d-4a77-a1a1-3fdfff114511" },
+      update: {},
+      create: {
+        id: "af0f6b40-d98d-4a77-a1a1-3fdfff114511",
+        name: "Plato",
+        username: "plato",
+        email: "plato@athenas.com",
+        birthdate: dayjs("1999-11-11").utc().format(),
+        levelStateId: "af0f6b40-d98d-4a77-a1a1-3fdfff114511",
+        profilePictureId: "af0f6b40-d98d-4a77-a1a1-3fdfff114511",
+        password: await bcrypt.hash(
+          "zonpoNWDNIEONAwqplaźwdqwd-20@(1314sda21342",
+          10
+        ),
+      },
+    }),
+  ];
 
-export {setupUsers};
+  await Promise.all(promises);
+};
+
+export { setupUsers };
